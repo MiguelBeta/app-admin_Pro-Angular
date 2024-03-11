a15044f0907490f0f2fc0f18f6e8549a2f8ffd84
Miguel <workingbeta01@gmail.com>
2024-03-11 18:11:33 -0500

    Componentes del ProgressBar y atributos personalizados

---
+++ src/app/components/incrementador/incrementador.component.ts
@@ -0,0 +1,34 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-incrementador',
+  templateUrl: './incrementador.component.html',
+  styles: ``
+})
+export class IncrementadorComponent {
+
+  @Input()
+  progreso : number = 50;
+
+  // get getPorcentaje() {
+  //   return `${ this.progreso }%`;
+  // }
+
+  cambiarValor( valor: number ): void {
+
+    if ( this.progreso >= 100 && valor >= 0  ){
+      this.progreso = 100;
+      return;
+    }
+
+    if ( this.progreso <= 0 && valor < 0 ){
+      this.progreso = 0;
+      return;
+    }
+
+    this.progreso = this.progreso + valor;
+    console.log( this.progreso );
+  }
+
+
+}
