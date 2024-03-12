1300739fe5bd0e77471fac9f69d19157f35efef5
Miguel <workingbeta01@gmail.com>
2024-03-12 11:38:54 -0500

    Detalles de incrementador y componentes padre e hijo

--- src/app/components/incrementador/incrementador.component.ts
+++ src/app/components/incrementador/incrementador.component.ts
@@ -1,33 +1,54 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-incrementador',
   templateUrl: './incrementador.component.html',
   styles: ``
 })
-export class IncrementadorComponent {
+export class IncrementadorComponent implements OnInit {
 
-  @Input()
-  progreso : number = 50;
+  ngOnInit(): void {
+    this.btnClass = `btn ${ this.btnClass }`;
+  }
 
-  // get getPorcentaje() {
-  //   return `${ this.progreso }%`;
-  // }
+  @Input('valor') progreso : number = 50;
+  @Input() btnClass : string = 'btn-primary';
+
+
+  @Output('valor') valorSalida: EventEmitter<number> = new EventEmitter();
 
   cambiarValor( valor: number ): void {
 
     if ( this.progreso >= 100 && valor >= 0  ){
+      this.valorSalida.emit( 100 );
       this.progreso = 100;
       return;
     }
 
     if ( this.progreso <= 0 && valor < 0 ){
+      this.valorSalida.emit( 0 );
       this.progreso = 0;
       return;
     }
 
     this.progreso = this.progreso + valor;
-    console.log( this.progreso );
+    this.valorSalida.emit( this.progreso );
+  }
+
+  onChange( nuevoValor: number ){
+
+    if( nuevoValor >= 100 ){
+      this.progreso = 100;
+    }
+    else if( nuevoValor <= 0 ){
+      this.progreso = 0;
+    } else{
+      this.progreso = nuevoValor;
+    }
+
+    this.valorSalida.emit( this.progreso );
+
+
   }
 
 
