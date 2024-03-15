70ff57d6a227154a4e110b5fdd606b3d279740de
Miguel <workingbeta01@gmail.com>
2024-03-14 19:04:24 -0500

    Interfaz page Grafica 1 creada con libreria chart.js

--- src/app/pages/grafica1/grafica1.component.ts
+++ src/app/pages/grafica1/grafica1.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import Chart, { ChartType } from 'chart.js/auto';
+
 
 @Component({
   selector: 'app-grafica1',
   templateUrl: './grafica1.component.html',
   styles: ``
 })
-export class Grafica1Component {
+export class Grafica1Component implements OnInit {
+
+  public chart: Chart | undefined;
+  public hola: string[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales'];
+
+  ngOnInit(): void {
+    this.generateChart();
+  }
+
+  generateChart(): void {
+    const data = {
+
+
+      labels: this.hola,
+
+      datasets: [{
+        hola: ['Download Sales', 'In-Store Sales', 'Mail-Order Sales',] as any[],
+        data: [350, 450, 100],
+        backgroundColor: [
+            '#6857E6',
+            '#009EEE',
+            '#F02059'
+        ],
+
+        hoverOffset: 4
+      }]
+    };
+
+    this.chart = new Chart("chart", {
+      type: 'pie' as ChartType,
+      data
+    })
+
+  }
+
 
 }
+
+
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-
 import { FormsModule } from '@angular/forms';
+
+import { BaseChartDirective } from 'ng2-charts';
+
 import { ComponentsModule } from './../components/components.module';
+import { SharedModule } from '../shared/shared.module';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Grafica1Component } from './grafica1/grafica1.component';
 import { PagesComponent } from './pages.component';
-import { SharedModule } from '../shared/shared.module';
 
 
 
@@ -30,6 +32,7 @@ import { SharedModule } from '../shared/shared.module';
   imports: [
     CommonModule,
     FormsModule,
+    BaseChartDirective,
     SharedModule,
     RouterModule,
     ComponentsModule
