58ed25a9bf552a48b2918f9e304223dbb8e1e940
Miguel <workingbeta01@gmail.com>
2024-03-30 16:51:41 -0500

    Token guardado en el local store y la funcion remember

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -3,6 +3,7 @@ import { UsuarioService } from '../../services/usuario.service';
 import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import Swal from 'sweetalert2';
+import { LoginForm } from '../../interfaces/login-form.interfaces';
 
 @Component({
   selector: 'app-login',
@@ -15,29 +16,47 @@ export class LoginComponent {
   public formSubmitted = false;
 
   public loginForm = this.fb.group({
-    email:    ['total@gmail.com', Validators.email],
-    password: ['123456', Validators.required],
-    remember: [ false ] ,
-
+    email: [ localStorage.getItem('email') || '', [Validators.required, Validators.email]],
+    password: ['', Validators.required],
+    remember: [false]
   });
 
-  constructor(  private router: Router,
-                private fb: FormBuilder,
-                private usuarioService: UsuarioService ) { }
-
-  login(){
-
-    // console.log( this.loginForm.value );
-    this.usuarioService.login( this.loginForm.value )
-      .subscribe( resp => {
-      console.log(resp)
-      }, (err) => {
-        // Alert si sucede un error
-        Swal.fire( 'Error', err.error.msg, 'error' ) ;
-      });
+  constructor(private router: Router,
+    private fb: FormBuilder,
+    private usuarioService: UsuarioService) { }
 
 
-  }
+login() {
+
+  // Obtener los valores del formulario
+  const email = this.loginForm.value.email ?? '';
+  const password = this.loginForm.value.password;
+  const remember = this.loginForm.value.remember;
+
+  // Verificar si los valores son nulos o indefinidos
+  const formData: LoginForm = {
+    email: this.loginForm.get('email')?.value ?? '',
+    password: this.loginForm.get('password')?.value ?? '',
+    remember: this.loginForm.get('remember')?.value ?? false
+  };
+
+  // Llamar el metodo login del servicio
+  this.usuarioService.login(formData)
+    .subscribe(resp => {
+      if( formData.remember ) {
+        localStorage.setItem('email', formData.email);
+        } else {
+          localStorage.removeItem('email');
+        }
+
+    }, (err) => {
+      // Alert si sucede un error
+      Swal.fire('Error', err.error.msg, 'error');
+    });
+
+
+
+}
 
 
 }
--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -1,8 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { RegisterForm } from '../interfaces/register-form.interfaces';
+import { tap } from "rxjs/operators";
+
 import { environment } from '../../environment.ts/environmet';
-import { LoginForm } from '../interfaces/login-form.interface';
+
+import { RegisterForm } from '../interfaces/register-form.interfaces';
+import { LoginForm } from '../interfaces/login-form.interfaces';
 
 const base_url = environment.base_url;
 
@@ -18,14 +21,24 @@ export class UsuarioService {
   creandoUsuario( formData: RegisterForm ){
 
     // peticion http tipo post
-    return this.http.post(`${ base_url }/usuarios`, formData );
+    return this.http.post(`${ base_url }/usuarios`, formData )
+              .pipe(
+                tap( ( resp: any ) => {
+                  localStorage.setItem('token', resp.token)
+                })
+              )
 
   }
 
   login( formData: LoginForm ){
 
     // peticion http tipo post
-    return this.http.post(`${ base_url }/login`, formData );
+    return this.http.post(`${ base_url }/login`, formData )
+               .pipe(
+                  tap( ( resp: any ) => {
+                    localStorage.setItem('token', resp.token)
+                  })
+                )
 
   }
 
